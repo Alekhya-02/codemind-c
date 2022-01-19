b8ca03c2e7561acd17bb9d04653471d38399ad2e
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2022-01-19 11:46:53 +0530

    #include<iostream> #include<bits/stdc++.h> using namespace std; int main() {     float x,y,result;     cin>>x>>y;     result=x*y;     cout<<fixed<<setprecision(2)<<result; }

---
+++ Program to Multiply Two Floating-Point Numbers.c
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float x,y,result;
+    cin>>x>>y;
+    result=x*y;
+    cout<<fixed<<setprecision(2)<<result;
+}

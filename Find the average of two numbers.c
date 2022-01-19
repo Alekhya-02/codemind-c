783d7fb13e87afcd15a2e917a7baacec4cbe2b8f
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2022-01-19 11:30:33 +0530

    #include<iostream> #include<bits/stdc++.h> using namespace std; int main() {     float x,y,avg;     cin>>x>>y;     avg=(x+y)/2;     cout << fixed << setprecision(4) << avg; }

---
+++ Find the average of two numbers.c
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float x,y,avg;
+    cin>>x>>y;
+    avg=(x+y)/2;
+    cout << fixed << setprecision(4) << avg;
+}

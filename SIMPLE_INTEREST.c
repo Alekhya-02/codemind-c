f903af35c6c7f3737c01355c37c44d8fe86ca5a8
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2022-01-25 10:49:26 +0530

    #include<iostream> using namespace std; int main() {     int p,t,r,si;     float result;     cin>>p>>t>>r;     result=(float)p*t*r/100;     si=(int)result;     cout<<si; }

---
+++ SIMPLE_INTEREST.c
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int p,t,r,si;
+    float result;
+    cin>>p>>t>>r;
+    result=(float)p*t*r/100;
+    si=(int)result;
+    cout<<si;
+}

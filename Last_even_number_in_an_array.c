2c0c68474ab0acaae6f2051ed9cbf3159e7cdb7a
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2022-04-30 20:00:48 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int n,a;     cin>>n;     vector<int>v;
        for(int i=0;i<n;i++)
        {
            cin>>a;
            v.emplace_back(a);
        }
        vector<int>w;
    
        for(int i=0;i<v.size();i++)
        {
            if(v[i]%2==0)
            {
                w.emplace_back(v[i]);
            }
        }
        
        cout<<w.back();
    }

---
+++ Last_even_number_in_an_array.c
@@ -0,0 +1,26 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,a;
+    cin>>n;
+    vector<int>v;
+    
+    for(int i=0;i<n;i++)
+    {
+        cin>>a;
+        v.emplace_back(a);
+    }
+    vector<int>w;
+
+    for(int i=0;i<v.size();i++)
+    {
+        if(v[i]%2==0)
+        {
+            w.emplace_back(v[i]);
+        }
+    }
+    
+    cout<<w.back();
+}
